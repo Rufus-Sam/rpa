073d37bf071b9ee7cb05177f79b0fec8452f051c
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-10-11 11:17:41 +0530

    error handler,also used async-express handler instead of try catch

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDb from './config/db.js'
 import videoRoutes from './routes/videoRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDb()
@@ -15,4 +16,8 @@ app.use('/api/videos', videoRoutes)
 
 const PORT = process.env.PORT || 5000
 const mode = process.env.NODE_ENV
+
+app.use(notFound)
+app.use(errorHandler)
+
 app.listen(PORT, console.log(`Server running on port ${PORT} in ${mode} mode`.yellow.bold))
